073382e5c9009aa2a98b778a28931ca6bde7a193
Israel Barreto <israelwnb@gmail.com>
2022-12-07 13:15:20 -0300

    Challenge complete!

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,19 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const user_id = request.get("user_id");
+
+        try {
+            const allUsers = this.listAllUsersUseCase.execute({ user_id });
+
+            return response.json(allUsers);
+        } catch (err) {
+            return response.status(400).send({ error: err.message });
+        }
+    }
 }
 
 export { ListAllUsersController };
